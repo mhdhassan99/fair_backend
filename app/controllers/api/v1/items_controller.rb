a3346aafeb6aceacd8dc2a6c281119078af12386
Muhammad Omar <momar2@wayfair.com>
2020-08-27 12:12:16 -0400

    asdsa

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,7 +1,10 @@
 class Api::V1::ItemsController < ApplicationController
     def index
         @items = Item.all
-        render json: @items
+        # @image_tray = @items.map { |item| item.image_tray.map{ |img| {itemId: img.record_id, imgUrl: url_for(img)}} }.flatten
+        # byebug
+        #  json: @items
+        render json: @items.to_json(methods: [:title])
     end
 
     def show
